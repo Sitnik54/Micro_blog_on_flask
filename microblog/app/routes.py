24240dddf1aa051c22e4820a6ac86698cf7c3da9
Sitnik54 <sitnik54@gmail.com>
2019-07-24 21:34:03 +0300

    add 404 and 500 errors html

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -9,7 +9,7 @@ from werkzeug.urls import url_parse
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -21,7 +21,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
-                           
+
 @app.route('/')
 @app.route('/index')
 @login_required
