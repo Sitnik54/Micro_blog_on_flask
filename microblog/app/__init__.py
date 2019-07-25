aa4044f9f722986fd6cfbbc72f0d9805c9e2f937
Sitnik54 <sitnik54@gmail.com>
2019-07-25 18:28:28 +0300

    add bootstrap

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
